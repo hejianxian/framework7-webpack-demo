e582e122d11b33e6e9f39ca76ce0e978a22361ab
Hejx <531601727@qq.com>
2017-02-26 15:37:39 +0800

    Add 'NODE_ENV' environment variable plugin.

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -41,6 +41,9 @@ const webpackConfig = {
 
 if (process.env.NODE_ENV === 'production') {
   config.plugins = config.plugins.concat([
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
